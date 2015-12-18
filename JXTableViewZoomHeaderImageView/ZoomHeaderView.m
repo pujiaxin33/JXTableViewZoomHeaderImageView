fc6cb390ee5bede54bea596062eae66109eecdf8
pujiaxin33 <www.317437084@qq.com>
2015-12-18 12:02:32 +0800

    更新标注

--- JXTableViewZoomHeaderImageView/ZoomHeaderView.m
+++ JXTableViewZoomHeaderImageView/ZoomHeaderView.m
@@ -79,6 +79,7 @@
     headerImageView.image = [UIImage imageNamed:@"lufei.jpg"];
     [self addSubview:headerImageView];
     self.headerImageView = headerImageView;
+    //约束设置为：跟父视图左、下、右贴紧，再约束高度，所以更新高度约束的时候会向上增加，xib约束同理
     [headerImageView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.right.and.bottom.equalTo(self).offset(0);
         self.codeConstraintHeight = make.height.equalTo(@(self.bounds.size.height));
@@ -94,15 +95,18 @@
 
 - (void)updateHeaderImageViewFrameWithOffsetY:(CGFloat)offsetY
 {
+    //用于实现向上滚动的时候，图片不变窄
     if (!self.isNeedNarrow && offsetY > 0) {
         return;
     }
     switch (self.type) {
         case ZoomHeaderViewTypeNoConstraint:
         {
+            //防止height小于0
             if (self.originalHeaderImageViewFrame.size.height - offsetY < 0) {
                 return;
             }
+            //如果不使用约束的话，图片的y值要上移offsetY,同时height也要增加offsetY
             CGFloat x = self.originalHeaderImageViewFrame.origin.x;
             CGFloat y = self.originalHeaderImageViewFrame.origin.y + offsetY;
             CGFloat width = self.originalHeaderImageViewFrame.size.width;
@@ -112,11 +116,12 @@
             break;
         case ZoomHeaderViewTypeCodeConstraint:
         {
+            //防止height小于0
             if (self.originalHeaderImageViewHeight -offsetY < 0) {
                 return;
             }
             //第一种方式：获取到这个约束，直接对约束值修改
-//            self.codeConstraintHeight.equalTo(@(self.originalHeaderImageViewHeight -offsetY));
+            //self.codeConstraintHeight.equalTo(@(self.originalHeaderImageViewHeight -offsetY));
             //第二种方式：直接使用masonry提供的更新约束方法，其实原理是一样的
             [self.headerImageView mas_updateConstraints:^(MASConstraintMaker *make) {
                 make.height.equalTo(@(self.originalHeaderImageViewHeight -offsetY));
@@ -125,6 +130,7 @@
             break;
         case ZoomHeaderViewTypeXibConstraint:
         {
+            //防止height小于0
             if (self.originalHeaderImageViewHeight -offsetY < 0) {
                 return;
             }
